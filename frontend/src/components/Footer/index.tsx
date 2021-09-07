d32436248ce287d43376a35aca848741668dd4bf
gabriel121souza <gabriel121souza@gmail.com>
2021-09-06 22:17:32 -0300

    basic static components

---
+++ frontend/src/components/Footer/index.tsx
@@ -0,0 +1,13 @@
+const Footer = () => {
+    return(
+        <footer className="footer mt-auto py-3 bg-dark">
+        <div className="container">
+          <p className="text-light">App desenvolvido por <a href="https://github.com/gabriel121souza" target="_blank" rel="noreferrer">Gabriel de Souza</a></p>
+          <p className="text-light"><small><strong>Semana Spring React</strong><br/>
+            Evento promovido pela escola DevSuperior: <a href="https://www.instagram.com/iamgabrielrodri/" target="_blank" rel="noreferrer">@iamgabrielrodri</a></small></p>
+        </div>
+      </footer>
+    );
+}
+
+export default Footer;
