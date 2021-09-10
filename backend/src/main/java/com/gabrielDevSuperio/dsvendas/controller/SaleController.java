65c36f3124edafd7622f593c68ca3cce4c00ec9d
gabriel121souza <gabriel121souza@gmail.com>
2021-09-10 15:33:33 -0300

    pagination

---
+++ backend/src/main/java/com/gabrielDevSuperio/dsvendas/controller/SaleController.java
@@ -0,0 +1,35 @@
+package com.gabrielDevSuperio.dsvendas.controller;
+
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
+import org.springframework.http.ResponseEntity;
+import org.springframework.transaction.annotation.Transactional;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.gabrielDevSuperio.dsvendas.dto.SaleDTO;
+import com.gabrielDevSuperio.dsvendas.repositories.SellerRepository;
+import com.gabrielDevSuperio.dsvendas.services.SaleService;
+
+@RestController
+@RequestMapping(value ="/sales")
+public class SaleController {
+
+	@Autowired
+	private SaleService saleService;
+	
+	@Autowired
+	private SellerRepository sellerRepository;
+	
+	
+	@Transactional
+	@GetMapping
+	public ResponseEntity<Page<SaleDTO>> findAll(Pageable pageable){
+		sellerRepository.findAll();
+		Page<SaleDTO> list = saleService.findAll(pageable);
+		return ResponseEntity.ok(list);
+	}
+}
--- backend/src/main/java/com/gabrielDevSuperio/dsvendas/services/SaleService.java
+++ backend/src/main/java/com/gabrielDevSuperio/dsvendas/services/SaleService.java
@@ -1,13 +1,23 @@
 package com.gabrielDevSuperio.dsvendas.services;
 
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Page;
+import org.springframework.data.domain.Pageable;
 import org.springframework.stereotype.Service;
 
+import com.gabrielDevSuperio.dsvendas.dto.SaleDTO;
+import com.gabrielDevSuperio.dsvendas.entities.Sale;
 import com.gabrielDevSuperio.dsvendas.repositories.SaleRepository;
 
 @Service
 public class SaleService {
 	@Autowired
 	private SaleRepository saleRepository;
+	
+	
+	public Page<SaleDTO> findAll(Pageable pageable){
+		Page<Sale> result = saleRepository.findAll(pageable);
+		return result.map(x -> new SaleDTO(x));
+	}
 	
 }
