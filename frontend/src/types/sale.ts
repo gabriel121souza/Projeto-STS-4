e42993db6dbb0d192659acfaa285e5cfa59d4138
gabriel121souza <gabriel121souza@gmail.com>
2021-09-13 15:11:06 -0300

    DataTable integration

--- frontend/src/types/sale.ts
+++ frontend/src/types/sale.ts
@@ -1,11 +1,11 @@
-
+import { Seller } from "./seller"
 export type Sale = {
     id: number;
     visited: number;
     deals: number;
     amount: number;
     date: string;
-//    seller: Seller;
+    seller: Seller;
 }
 
 export type SalePage = {
