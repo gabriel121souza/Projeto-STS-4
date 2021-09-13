da533291009ce874226f56d94123e206b04608ad
gabriel121souza <gabriel121souza@gmail.com>
2021-09-13 14:43:31 -0300

    barChar integration

---
+++ frontend/src/components/DonutChart/index.tsx
@@ -0,0 +1,48 @@
+import axios from "axios";
+import Chart from "react-apexcharts";
+import { useState } from "react";
+import { useEffect } from "react";
+import { BASE_URL } from "utils/requests";
+import { SaleSum } from "types/sale";
+
+type ChatData = {
+    labels : string[]
+    series : number[]
+}
+
+function DonutChart() {
+
+    const [chatData, setChartData] = useState<ChatData>({ labels: [], series: []}); 
+
+    useEffect(()=> {
+        axios.get(`${BASE_URL}/sales/amount-by-seller`)
+        .then(response=> {
+
+            const data = response.data as SaleSum[];
+            const myLabels = data.map(x => x.sellerName);
+            const mySeries = data.map(x => x.sum);
+
+            setChartData({ labels: myLabels, series: mySeries});
+        })
+    } , []);
+    
+    const options = {
+        legend: {
+            show: true
+        }
+    }
+    
+
+    return (
+
+        <Chart
+            options={{...options, labels: chatData.labels}}
+            series={chatData.series}
+            type="donut"
+            height="240"
+        />
+
+    );
+}
+
+export default DonutChart;
---
+++ frontend/src/utils/requests.ts
@@ -0,0 +1,2 @@
+//export  const BASE_URL = 'http://localhost:8080';
+export  const BASE_URL = 'https://sds-4-gabriel-de-souza.herokuapp.com';
